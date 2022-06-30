5a3e24c7487f347547f721f5bf0a109a7f5aa84b
Emergin <ngangaephantus112@gmail.com>
2022-06-30 05:10:02 -0700

    my

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    arguments = (len(sys.argv) -1)
+    arguments = (len(sys.argv) - 1)
     if(arguments == 0):
         print("0 arguments.")
     elif(arguments > 0):
@@ -9,5 +9,5 @@ if __name__ == "__main__":
             print("{} argument:".format(arguments))
         else:
             print("{} arguments:".format(arguments))
-            for i in range(1, (arguments + 1)):
-                print("{}: {}".format(i, sys.argv[i]))
+        for i in range(1, (arguments + 1)):
+            print("{}: {}".format(i, sys.argv[i]))
