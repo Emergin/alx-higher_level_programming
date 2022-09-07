0e2b778afe395e0df076e0f5f6c329e4f621ed8d
Emergin <ngangaephantus112@gmail.com>
2022-09-07 02:33:34 -0700

    my

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,8 +1,7 @@
--- displays null for shows without genres
---records are ordered by ascending 
+-- Displays NULL for shows without genres.
+-- Records are ordered by ascending tv_shows.title and tv_show_genres.genre_id.
 SELECT s.`title`, g.`genre_id`
-	FROM `tv_shows` AS s
-		LEFT JOIN `tv_show_genres` AS g
-		ON s.`id` = g.`show_id`
-ORDER BY s.`title`, g.`genre_id`;
-
+  FROM `tv_shows` AS s
+       LEFT JOIN `tv_show_genres` AS g
+       ON s.`id` = g.`show_id`
+ ORDER BY s.`title`, g.`genre_id`;
